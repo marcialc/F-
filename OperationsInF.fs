ab55b9b9c0764e7b71086d72e097d4b77b588674
marcialc <marcialandres@me.com>
2018-01-22 19:32:33 -0500

    Add files via upload

---
+++ OperationsInF.fs
@@ -0,0 +1,27 @@
+﻿
+
+module Operations
+
+let divide x = x / 2
+
+let multiply y = y * 2
+
+let rec fib n =
+    match n with
+    | 1 | 2 -> 1
+    | n -> fib(n-1) + fib(n-2)
+ 
+[<EntryPoint>]
+
+let main argv =
+
+    printfn "Hello World"
+
+    printfn "%d divided by 2 is: %d" 10 (divide 10)
+
+    printfn "%d multiplied by 2 is: %d" 5 (multiply 5)
+
+    printfn "%d in Fibonacci is:  %d" 8 (fib 8)
+
+
+    0 // return an integer exit code
